93b123fba9d71bd38a4c0a7534c75982a1ba6d58
Richard Jones <richard@cottagelabs.com>
2015-11-23 12:11:28 +0000

    remove more unused code and config

--- octopus/config/googlemap.py
+++
@@ -1,2 +0,0 @@
-# Google Map API key
-GOOGLE_MAP_API_KEY = None
--- octopus/config/rootcfg.py
+++ octopus/config/rootcfg.py
@@ -4,24 +4,15 @@ CONFIG_FILES = [
     # octopus.lib config files
     "magnificent-octopus/octopus/config/cli.py",
     "magnificent-octopus/octopus/config/dates.py",
-    "magnificent-octopus/octopus/config/googlemap.py",
     "magnificent-octopus/octopus/config/http.py",
     "magnificent-octopus/octopus/config/mail.py",
     "magnificent-octopus/octopus/config/webapp.py",
 
     # octopus.module config files
     "magnificent-octopus/octopus/modules/account/settings.py",
-    "magnificent-octopus/octopus/modules/cache/settings.py",
     "magnificent-octopus/octopus/modules/clientjs/settings.py",
-    "magnificent-octopus/octopus/modules/coreacuk/settings.py",
-    "magnificent-octopus/octopus/modules/crud/settings.py",
-    "magnificent-octopus/octopus/modules/doaj/settings.py",
-    "magnificent-octopus/octopus/modules/epmc/settings.py",
     "magnificent-octopus/octopus/modules/es/settings.py",
     "magnificent-octopus/octopus/modules/jper/settings.py",
-    "magnificent-octopus/octopus/modules/oag/settings.py",
-    "magnificent-octopus/octopus/modules/romeo/settings.py",
-    "magnificent-octopus/octopus/modules/sherpafact/settings.py",
     "magnificent-octopus/octopus/modules/store/settings.py",
     "magnificent-octopus/octopus/modules/swordv2/settings.py",
 
@@ -40,8 +31,7 @@ TEMPLATE_PATHS = [
 
     # octopus modules templates
     "magnificent-octopus/octopus/modules/account/templates",
-    "magnificent-octopus/octopus/modules/clientjs/templates",
-    "magnificent-octopus/octopus/modules/oag/templates"
+    "magnificent-octopus/octopus/modules/clientjs/templates"
 ]
 
 # absolute paths, or relative paths from the root directory, to the static file directories (in the order you want them looked at)
@@ -55,11 +45,7 @@ STATIC_PATHS = [
     # octopus modules static directories
     "magnificent-octopus/octopus/modules/account/static",
     "magnificent-octopus/octopus/modules/clientjs/static",
-    "magnificent-octopus/octopus/modules/crud/static",
-    "magnificent-octopus/octopus/modules/es/static",
-    "magnificent-octopus/octopus/modules/form/static",
-    "magnificent-octopus/octopus/modules/oag/static",
-    "magnificent-octopus/octopus/modules/sherpafact/static"
+    "magnificent-octopus/octopus/modules/es/static"
 ]
 
 # module import paths for the app initialisation modules that need to run at flask app creation
